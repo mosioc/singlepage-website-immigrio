ed351cbc3f3ccbeb02b8fcff11bb6767b9e304c6
Mehdi Maleki <mosioc79@gmail.com>
2025-11-16 20:48:13 +0400

    add bone structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React from 'react';
+import styles from './style.js';
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
     <div>
-
-    
-
+      {/* // navbar section */}
+      <div className={`bg-red-50 ${styles.paddingX} ${styles.paddingY} ${styles.boxWidth}`}>
+        test
+      </div>
+      {/* // promo section */}
+      <div className={`${styles.flexCenter}`}></div>
+      {/* // stats section */}
+      <div className={`${styles.flexCenter}`}></div>
+      {/* // sales section */}
+      <div></div>
+      {/* // promo 2 section */}
+      <div></div>
+      {/* // testimonials section */}
+      <div></div>
+      {/* // call to action section */}
+      <div></div>
+      {/* // footer section */}
+      <div></div>
     </div>
   );
 };
